39643a04beb8c9f622b0eee467e23bdf7b90a661
DanqingYANG <lenayoungydq@gmail.com>
2015-11-01 00:13:54 +0100

    add the class MusicFiles to open a folder with airplay

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tools.MusicFiles playlist = new MusicFiles();
+            playlist.test();
+            int a = 0;
+        }
+    }
+}
